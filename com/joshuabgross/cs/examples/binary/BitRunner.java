9a585c4dac61f07ed2e32ed8caff81cee965f1b9
jbgross <gross.joshua.b@gmail.com>
2018-11-25 15:38:38 -0600

    added a quick class to run bitshifting data

---
+++ com/joshuabgross/cs/examples/binary/BitRunner.java
@@ -0,0 +1,13 @@
+package com.joshuabgross.cs.examples.binary;
+
+/**
+ *
+ * @author joshua.gross
+ */
+public class BitRunner {
+    public static void main(String[] args) {
+        // randomly generate and manipulate 10 numbers
+        new BitShifting(10);
+    }
+    
+}
